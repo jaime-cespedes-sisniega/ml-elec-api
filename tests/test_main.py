a85445adfa9634b014bb0ff16ce6013dbd043563
Jaime Céspedes Sisniega <jaime.cespedes.sisniega@gmail.com>
2021-11-23 10:33:44 +0100

    Add health endpoint test

---
+++ tests/test_main.py
@@ -0,0 +1,11 @@
+from fastapi.testclient import TestClient
+
+from api.main import api
+
+client = TestClient(api)
+
+
+def test_health():
+    response = client.get("/health")
+    assert response.status_code == 200
+    assert response.json() == {'Status': 'Ok!'}
