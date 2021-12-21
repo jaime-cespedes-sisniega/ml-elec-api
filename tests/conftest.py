7173e52832b7b9aec1147ac79fc54070e22ec674
Jaime Céspedes Sisniega <jaime.cespedes.sisniega@gmail.com>
2021-12-21 12:41:17 +0100

    Add conftest.py

---
+++ tests/conftest.py
@@ -0,0 +1,38 @@
+from fastapi.testclient import TestClient
+import pytest
+
+
+# Patch model registry connection to avoid having a
+# database running to run the tests
+@pytest.fixture(scope='module')
+def client(module_mocker):
+    module_mocker.patch('app.config.Settings')
+    module_mocker.patch('app.utils.load_model')
+    from app.main import app
+    with TestClient(app) as test_client:
+        yield test_client
+
+
+@pytest.fixture(scope='module')
+def input_data():
+    return {
+        "inputs": [
+            {
+                "day": 3,
+                "period": 0.02,
+                "nswprice": 0.05,
+                "nswdemand": 0.43,
+                "vicprice": 0.003,
+                "vicdemand": 0.42,
+                "transfer": 0.41
+            }
+        ]
+    }
+
+
+@pytest.fixture()
+def input_data_missing_feature(input_data):
+    input_data_missing = input_data.copy()
+    input_data_missing['inputs'][0].pop('period')
+
+    return input_data_missing
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,35 +1,10 @@
 from app import __version__, schemas
 from app.config import settings
-import pytest
-from fastapi.testclient import TestClient
 
 
-# Patch model registry connection to avoid having a
-# database running to run the tests
-@pytest.fixture(scope='module')
-def client(module_mocker):
-    module_mocker.patch('app.config.Settings')
-    module_mocker.patch('app.utils.load_model')
-    from app.main import app
-    with TestClient(app) as test_client:
-        yield test_client
-
-
-input_data = {"inputs": [
-        {
-            "day": 3,
-            "period": 0.02,
-            "nswprice": 0.05,
-            "nswdemand": 0.43,
-            "vicprice": 0.003,
-            "vicdemand": 0.42,
-            "transfer": 0.41
-        }
-    ]
-}
-
-
-def test_predict_correct_output(client, mocker):
+def test_predict_correct_output(client,
+                                input_data,
+                                mocker):
     fake_pred = ['DOWN']
     mock_make_prediction = mocker.patch('app.api.make_prediction',
                                         return_value=fake_pred)
@@ -40,12 +15,13 @@ def test_predict_correct_output(client, mocker):
     assert mock_make_prediction.call_args.kwargs['input_data'] == input_data
 
 
-def test_predict_missing_feature(client):
-    data_input_missing = input_data.copy()
-    data_input_missing['inputs'][0].pop('period')
+def test_predict_missing_feature(client,
+                                 input_data_missing_feature):
     response = client.post('/api/v1/predict',
-                           json=data_input_missing)
+                           json=input_data_missing_feature)
     assert response.status_code == 422
+    assert response.text == ('{"detail":[{"loc":["body","inputs",0,"period"],'
+                             '"msg":"field required","type":"value_error.missing"}]}')
 
 
 def test_health(client):
